f2968fcfc055794b7e46b8c3f6f4abf37de8248a
ponyovsa <Y230219@mail.ryukoku.ac.jp>
2026-01-31 18:35:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ import time
 'Starting a long computation...'
 
 # Add a placeholder
+if 'current_seconds' not in st.session_state:
+    st.session_state.current_seconds = 0
 
- 
 time_set = st.slider("Set time (minutes)", 1, 60, 25)
 rest_time = st.slider("Set rest time (minutes)", 1, 30, 5)
 
@@ -13,7 +14,8 @@ latest_iteration = st.empty()
 bar = st.progress(0)
 
 if st.button("Start Timer"):
-    for i in range(time_set * 60):
+    for i in range(st.session_state.current_seconds, time_set * 60):
+        st.session_state.current_seconds = i
         latest_iteration.text(f'Iteration {time_set * 60 - i}')
         bar.progress((time_set * 60 - i) / (time_set * 60))
         time.sleep(1)
@@ -22,3 +24,5 @@ if st.button("Start Timer"):
         latest_iteration.text(f'Rest Time {rest_time * 60 - j}')
         bar.progress((rest_time * 60 - j) / (rest_time * 60))
         time.sleep(1)
+
+    st.session_state.current_seconds = 0
