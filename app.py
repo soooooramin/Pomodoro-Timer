4b3f8d530900111948eb7b4d79adbb09468c6083
soooooramin <mdaggss0@gmail.com>
2026-01-31 18:25:40 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import time
+
+'Starting a long computation...'
+ 
+time_set = st.slider("Set time (minutes)", 1, 60, 25)
+rest_time = st.slider("Set rest time (minutes)", 1, 30, 5)
+
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+if st.button("Start Timer"):
+    for i in range(time_set * 60):
+        latest_iteration.text(f'Iteration {time_set * 60 - i}')
+        bar.progress((time_set * 60 - i) / (time_set * 60))
+        time.sleep(1)
+    
+    for j in range(rest_time * 60):
+        latest_iteration.text(f'Rest Time {rest_time * 60 - j}')
+        bar.progress((rest_time * 60 - j) / (rest_time * 60))
+        time.sleep(1)
+
+
